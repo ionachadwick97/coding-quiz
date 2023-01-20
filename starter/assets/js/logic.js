590be7ee315f774ffd68c2c70415f0644529c312
ionachadwick97 <117356506+ionachadwick97@users.noreply.github.com>
2023-01-20 19:52:29 +0000

    Update logic.js

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,4 +1,4 @@
-// 1. Start button that goes to first question when clicked.
+/ 1. Start button that goes to first question when clicked.
 
 // 2. Multiple choice questions with buttons for each answer.
 
@@ -13,72 +13,90 @@
 // 7. Submit button goes to highscores.
 
 
-
-// Prepare the questions in question.js file
-// var questions = ...
-var score = 0;
-var currentQuestion = 0;
-var counter;
-var timer;
-
-// Prepare all selector that we might need to point to the html element
+// variable declarations
 var startButton = document.querySelector('#start');
 var startScreen = document.querySelector("#start-screen");
 var questionsContainer = document.querySelector("#questions");
-var questionsTitle = document.querySelector("#question-title");
+var questionTitle = document.querySelector("#question-title");
 var choicesContainer = document.querySelector("#choices");
 var timerContainer = document.querySelector("#timer");
-// ....
+var endScreenContainer = document.querySelector("#end-screen");
+var finalScoreContainer = document.querySelector("#final-score");
+var score = 0;
+
+
+// questions array
+var title = [];
+for (let i = 0; i<questions.length; i++) {
+    tempTitles = questions[i].title;
+    title.push(tempTitles)
+}
+
+
+// choices array
+var choices = [];
+for (let i = 0; i<questions.length; i++) {
+    tempChoices = questions[i].choices;
+    choices.push(tempChoices)
+}
+
 
 // start button && timer
 startButton.addEventListener('click', function() {
     startScreen.setAttribute('class', 'hide');
     questionsContainer.setAttribute('class', 'visible');
 
-    questions.forEach(function(question) {
-        
-    });
-    // show the first question
-    currentQuestion = 0;
-    populateQuestion(questionsContainer[currentQuestion]);
 
-    counter = 100;
-    timer = setInterval(function() {
-        counter--;
-        // set timerContainer text to counter
-        if (counter <= 0) {
-            endGame()
-            clearInterval(timer);
-        }
-    }, 1000);
+
+     populateQuestion();
+
+    //  var counter = 300;
+    //  var timer = setInterval(function() {
+    //      counter--;
+    //      timerContainer.textContent = counter;
+    //      if (counter <= 0) {
+    //          endGame()
+    //          clearInterval(timer);
+    //      }
+    //  }, 1000);
 });
 
 
+//populate question
 function populateQuestion(question) {
-    var question = question.title;
-    var choices = question.choices;
-    var answer = question.answer;
-
     choicesContainer.innerHTML = '';
-    questionTitle.textContent = question;
+    questionTitle.textContent = title[0];
     var choicesList = document.createElement('ul');
-    for (let i = 0; i < choices.length; i++) {
+    for (let i = 0; i < 4; i++) {
         var choice = document.createElement('li');
-        choice.textContent = choices[i];
+        choice.textContent = choices[0][i];
         choicesList.appendChild(choice);
     }
-    choicesContainer.appendChild(choicesList)
+    choicesContainer.appendChild(choicesList);
 }
 
+li.addEventListener("click", answer) {
+
+    
+}
+
+// question results
+
+
+
+
+//end game
 function endGame() {
     // When the game ends, it should display their score and give the user the ability to save their initials and their score
 
-    // hide questions container
-    // show endScreen container
-    // assign score to finalScore container
-    // reset the timer clearInterval(timer);
+     questionsContainer.setAttribute("class", "hide");
+     endScreenContainer.setAttribute("class", "visible");
+     finalScoreContainer.textContent = score;
+     clearInterval(timer);
 }
 
+
+//next question
 function nextQuestion() {
     currentQuestion++;
     if (currentQuestion < questions.length) {
@@ -89,18 +107,21 @@ function nextQuestion() {
 }
 
 
+//save high score
 function saveHighscore(initial) {
     // get the current highscores value from localstorage
-    // json parse current highscores from localstorage, this will be an array of object
+    // json parse current highscores from localstorage, this will be an array of objects
     // push initial + score to the array
     // order the array from highest score to lowest
     // json stringify then save back to localstorage
 }
 
-// Another click event listener for choices
+
+// Another click event listener on li for choices
 //    Check answer
 //        if correct, add 1 to score, call nextQuestion()
 //        if wrong, remove 10 seconds from the interval, call nextQuestion()
+
 
 // Click event listener to submit button
 //    var initial = initialInput.value.trim()
